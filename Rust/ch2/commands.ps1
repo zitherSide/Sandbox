3885fb6fcfcdf4ca478c75fd62e33edc9bd40923
Takuya Kotoyori <takuya.kotoyori@woven-planet.global>
2022-08-13 10:19:32 +0900

    Added simple server

--- Rust/ch2/commands.ps1
+++ Rust/ch2/commands.ps1
@@ -1,7 +1,9 @@
-cargo --version
-rustc --version
-rustdoc --version
+#cargo --version
+#rustc --version
+#rustdoc --version
 # cargo new --bin hello
 # cargo run
-cargo test
+#cargo test
 #cargo clean
+
+cargo new --bin iron-gcd
