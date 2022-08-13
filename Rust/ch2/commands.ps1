ed8a5976d7102c61f65987685b3bc419efc22a9e
zitherSide <takuyakotoyori@akane.waseda.jp>
2022-08-13 17:19:59 +0900

    added mandelbrot

--- Rust/ch2/commands.ps1
+++ Rust/ch2/commands.ps1
@@ -6,4 +6,9 @@
 #cargo test
 #cargo clean
 
-cargo new --bin iron-gcd
+#cargo new --bin iron-gcd
+
+# cargo new --bin mandelbrot
+
+# cargo build --release
+Measure-Command {./target/release/mandelbrot mandel.png 4000x3000 -1.20,0.35 -1.0,0.20}
