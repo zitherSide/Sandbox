740ae090105daff11345193518fff7eb6a0a4ac4
zitherSide <takuyakotoyori@akane.waseda.jp>
2022-10-20 12:46:13 +0900

    set up debug env

--- Electron/sample_app/index.js
+++ Electron/sample_app/index.js
@@ -1,14 +1,39 @@
 const {app, BrowserWindow} = require('electron');
 
 function createWindow(){
-    let win = new BrowserWindow({
-        width: 400,
-        height: 200,
+    const win = new BrowserWindow({
+        width: 500,
+        height: 400,
+        backgroundColor: '#660066',
         webPreferences: {
             nodeIntegration: true
         }
     })
     win.loadFile('index.html')
+    // win.loadURL('https://www.tuyano.com')
+    win.webContents.openDevTools()
+
+    const child1 = new BrowserWindow({
+        width: 350,
+        height: 200,
+        parent: win,
+        frame: false,
+        transparent: true,
+        // modal: true,
+        webPreferences:{
+            nodeIntegration: true
+        }
+    })
+    // child.loadURL('https://www.tuyano.com')
+    child1.loadFile('sub.html')
+
+    const child2 = new BrowserWindow({
+        width: 350,
+        height: 250,
+        parent: win,
+        opacity: 0.5
+    })
+    child2.loadFile('sub.html')
 }
 
 app.whenReady().then(createWindow)
